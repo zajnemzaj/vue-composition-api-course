cfe0e0be6a90565a905e120e7c786de875f3f47f
zajnemzaj <dezsoj@gmail.com>
2024-01-04 00:02:59 +0100

    073 - router add some routes

--- vue-composition-api-noteballs/src/main.js
+++ vue-composition-api-noteballs/src/main.js
@@ -3,10 +3,23 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
+import ViewNotes from '@/views/ViewNotes.vue'
+import ViewStats from '@/views/ViewStats.vue'
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'notes',
+      component: ViewNotes
+    },
+    {
+      path: '/stats',
+      name: 'stats',
+      component: ViewStats
+    }
+  ]
 })
 
 createApp(App).use(router).mount('#app')

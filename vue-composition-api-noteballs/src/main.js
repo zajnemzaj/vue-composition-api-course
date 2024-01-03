3e0e71fa28e8e421c44611f5319b0606a9217e5e
zajnemzaj <dezsoj@gmail.com>
2024-01-03 23:57:04 +0100

    072 - router install setup

--- vue-composition-api-noteballs/src/main.js
+++ vue-composition-api-noteballs/src/main.js
@@ -1,6 +1,12 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: []
+})
+
+createApp(App).use(router).mount('#app')

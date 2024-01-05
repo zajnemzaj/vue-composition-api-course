ce7563d35ffc2c466a9b06c88c9c5f63e0a2e809
zajnemzaj <dezsoj@gmail.com>
2024-01-05 19:07:29 +0100

    092 - pinia setup state - also disabling default main css to have bulma working properly

--- vue-composition-api-noteballs/src/main.js
+++ vue-composition-api-noteballs/src/main.js
@@ -1,7 +1,11 @@
-import './assets/main.css'
+// import './assets/main.css'
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 // not needed to specify the index.js file as it automaticaly looks for that
 import router from '@/router'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(createPinia())
+  .use(router)
+  .mount('#app')
---
+++ vue-composition-api-noteballs/src/stores/storeNotes.js
@@ -0,0 +1,18 @@
+import { defineStore } from 'pinia'
+
+export const useStoreNotes = defineStore('storeNotes', {
+  state: () => {
+    return { 
+      notes: [
+        {
+          id: 'id1',
+          content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque consequatur soluta modi voluptatibus quidem libero repellendus quod nobis nemo qui architecto necessitatibus at nostrum, aut voluptates! Beatae rem eaque eos.'
+        },
+        {
+          id: 'id2',
+          content: 'Shorter note.'
+        }
+      ] 
+    }
+  }
+})

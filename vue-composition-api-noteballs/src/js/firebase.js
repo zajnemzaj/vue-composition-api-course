af880a1279a0780e4fe6955dacb9b23699de7ef4
zajnemzaj <dezsoj@gmail.com>
2024-01-27 12:48:29 +0100

    124 - setup firestore database

--- vue-composition-api-noteballs/src/js/firebase.js
+++ vue-composition-api-noteballs/src/js/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyA-u8mDQ-Pxky3ElWMSWmc_32mjrRVDfcc',
@@ -12,3 +13,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export {
+  db
+}

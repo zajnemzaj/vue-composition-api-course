c5c999af25d0d0c4615a0f81d7b63aa90dc80ebf
zajnemzaj <dezsoj@gmail.com>
2024-01-27 13:00:57 +0100

    126 - display notes from firestore

--- vue-composition-api-noteballs/src/stores/storeNotes.js
+++ vue-composition-api-noteballs/src/stores/storeNotes.js
@@ -1,22 +1,33 @@
 import { defineStore } from 'pinia'
+import { collection, getDocs } from 'firebase/firestore'
 import { db } from '@/js/firebase'
 
 export const useStoreNotes = defineStore('storeNotes', {
   state: () => {
     return { 
       notes: [
-        {
-          id: 'id1',
-          content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque consequatur soluta modi voluptatibus quidem libero repellendus quod nobis nemo qui architecto necessitatibus at nostrum, aut voluptates! Beatae rem eaque eos.'
-        },
-        {
-          id: 'id2',
-          content: 'Shorter note.'
-        }
+        // {
+        //   id: 'id1',
+        //   content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque consequatur soluta modi voluptatibus quidem libero repellendus quod nobis nemo qui architecto necessitatibus at nostrum, aut voluptates! Beatae rem eaque eos.'
+        // },
+        // {
+        //   id: 'id2',
+        //   content: 'Shorter note.'
+        // }
       ] 
     }
   },
   actions: {
+    async getNotes() {
+      const querySnapshot = await getDocs(collection(db, 'notes'))
+      querySnapshot.forEach((doc) => {
+      let note = {
+        id: doc.id,
+        content: doc.data().content
+      }
+      this.notes.push(note)
+      })
+    },
     addNote(newNoteContent) {
       let currentDate = new Date().getTime(),
           id = currentDate.toString()

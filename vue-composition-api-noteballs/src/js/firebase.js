8a33b0456db8c51432aaadf1307f482481e8ac9b
zajnemzaj <dezsoj@gmail.com>
2024-01-27 12:40:44 +0100

    123 - create app install firebase - base configuration

---
+++ vue-composition-api-noteballs/src/js/firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA-u8mDQ-Pxky3ElWMSWmc_32mjrRVDfcc',
+  authDomain: 'noteballs-f42f4.firebaseapp.com',
+  projectId: 'noteballs-f42f4',
+  storageBucket: 'noteballs-f42f4.appspot.com',
+  messagingSenderId: '734194070551',
+  appId: '1:734194070551:web:326b474aa8fdbd13ce9852'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)

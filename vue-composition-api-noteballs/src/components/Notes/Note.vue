fb71093d97b84add2f1c99a3a19a8ca228cfba0a
zajnemzaj <dezsoj@gmail.com>
2024-02-20 00:24:39 +0100

    133 - display date on note

--- vue-composition-api-noteballs/src/components/Notes/Note.vue
+++ vue-composition-api-noteballs/src/components/Notes/Note.vue
@@ -5,8 +5,9 @@
     <div class="card-content">
       <div class="content">
         {{ note.content }}
-        <div class="has-text-right has-text-grey-light mt-2">
-          <small>{{ characterLength }}</small>
+        <div class="columns is-mobile has-text-grey-light mt-2">
+          <small class="column has-text-left">{{ dateFormated }}</small>
+          <small class="column has-text-right">{{ characterLength }}</small>
         </div>
       </div>
     </div>
@@ -34,6 +35,7 @@
 */
 
 import { computed, reactive } from 'vue'
+import { useDateFormat } from '@vueuse/core'
 import ModelDeleteNote from "@/components/Notes/ModalDeleteNote.vue"
 import { useStoreNotes } from '@/stores/storeNotes'
 
@@ -53,6 +55,16 @@ const props = defineProps({
 */
 
 const storeNotes = useStoreNotes()
+
+/**
+ * date formated
+ */
+
+const dateFormated = computed(() => {
+  let date = new Date(parseInt(props.note.date))
+  let formattedDate = useDateFormat(date, 'YYYY.MM.DD. @ HH:mm')
+  return formattedDate.value
+})
 
 /*
   character length

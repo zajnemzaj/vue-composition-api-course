3acb7c312fa02d3ea4624f8495e17f9ae08d179b
zajnemzaj <dezsoj@gmail.com>
2024-03-05 22:19:57 +0100

    144 - redirect user on auth change - reaching out router from pinia's storeAuth.js

--- vue-composition-api-noteballs/src/main.js
+++ vue-composition-api-noteballs/src/main.js
@@ -1,11 +1,17 @@
 // import './assets/main.css'
-import { createApp } from 'vue'
+import { createApp, markRaw } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 // not needed to specify the index.js file as it automaticaly looks for that
 import router from '@/router'
 
+const pinia = createPinia()
+
+pinia.use(({ store }) => {
+  store.router = markRaw(router)
+})
+
 createApp(App)
-  .use(createPinia())
+  .use(pinia)
   .use(router)
   .mount('#app')

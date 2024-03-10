6320a33b83226d3c7df8cd7677356615e53e7253
zajnemzaj <dezsoj@gmail.com>
2024-03-10 12:29:33 +0100

    148 - unsubscripbe from the get notes listener so only own notes are shown when added a new one

--- vue-composition-api-noteballs/src/stores/storeNotes.js
+++ vue-composition-api-noteballs/src/stores/storeNotes.js
@@ -10,6 +10,8 @@ import { useStoreAuth } from '@/stores/storeAuth'
 let notesCollectionRef
 let notesCollectionQuery
 
+let getNotesSnapshot = null
+
 export const useStoreNotes = defineStore('storeNotes', {
   state: () => {
     return {
@@ -39,7 +41,8 @@ export const useStoreNotes = defineStore('storeNotes', {
     },
     async getNotes() {
       this.notesLoaded = false
-      onSnapshot(notesCollectionQuery, (querySnapshot) => {
+
+      getNotesSnapshot = onSnapshot(notesCollectionQuery, (querySnapshot) => {
         let notes = []
         querySnapshot.forEach((doc) => {
           let note = {
@@ -61,6 +64,9 @@ export const useStoreNotes = defineStore('storeNotes', {
     },
     clearNotes() {
       this.notes = []
+
+      // unsubscribe from any active listener
+      if (getNotesSnapshot) getNotesSnapshot()
     },
     async addNote(newNoteContent) {
       let currentDate = new Date().getTime(),
